408aad0f434461ebdc8acc6f0176a313d4af0c33
carlenee <carleneannabel@gmail.com>
2022-09-14 21:36:53 +0700

    Mengerjakan tutorial lab2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 data_barang_wishlist = BarangWishlist.objects.all()
+
 context = {
     'list_barang': data_barang_wishlist,
     'nama': 'Carlene Annabel'
@@ -10,3 +13,21 @@ context = {
 # Create your views here.
 def show_wishlist(request):
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
